a934cafb93ae5f933ec1b28fed5c01040f403766
Rishav Bharti <brishabh619@gmail.com>
2020-12-08 17:36:33 +0530

    first commit: this project was completely developed on local machine.

---
+++ app.js
@@ -0,0 +1,32 @@
+var translateBtn = document.querySelector('.translate-btn');
+
+var inputTxt = document.querySelector('.txt-input');
+
+var outputDiv = document.querySelector('#output');
+
+
+var serverurl = "https://api.funtranslations.com/translate/minion.json";
+
+
+// 
+
+
+function getTranslationUrl(input)  {
+    return serverurl + '?' + 'text=' + input
+}
+
+
+function translateTxt(){
+    var inputText = inputTxt.value;
+    fetch(getTranslationUrl(inputText))
+        .then(response=>response.json())
+        .then(json=>{
+            var bananaText = json.contents.translated;
+            outputHtml = `<h3><mark>${bananaText}</mark></h3>`
+            outputDiv.innerHTML = outputHtml;
+        })
+        .catch(err=>console.log(err));
+}
+
+
+translateBtn.addEventListener('click',translateTxt);
